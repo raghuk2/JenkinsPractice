13852c8ee802f4913c14bbe9c92b6441acc0d921
raghuk2 <39421611+raghuk2@users.noreply.github.com>
2020-06-06 10:40:38 -0500

    Added Arya and Rishi

--- Sample.java
+++ Sample.java
@@ -2,8 +2,8 @@ public class HelloGuys {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hey Agni");
-		System.out.println("Hey Riya");
+		System.out.println("Hey Arya");
+		System.out.println("Hey Rishi");
 		System.out.println("Hey Duck");
 		System.out.println("Hey Arya");//newly added
 		System.out.println("Hey Aishu");//newly added
