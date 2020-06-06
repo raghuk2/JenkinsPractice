b71c5b84c654a61ab0054e7ded56501ec062bbdd
raghuk2 <39421611+raghuk2@users.noreply.github.com>
2020-06-06 10:46:57 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ public class HelloGuys {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hey Arya");
+		System.out.println("Hey Aryaaaaa");
 		System.out.println("Hey Rishi");
 		System.out.println("Hey Duck");
 		System.out.println("Hey Arya");//newly added
