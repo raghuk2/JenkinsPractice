524c1289d95e14e3c46989fe9627e6079f7c98a6
raghuk2 <39421611+raghuk2@users.noreply.github.com>
2020-06-06 10:22:18 -0500

    Created new sample file to test jenkins

---
+++ Sample.java
@@ -0,0 +1,12 @@
+public class HelloGuys {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey Agni");
+		System.out.println("Hey Riya");
+		System.out.println("Hey Duck");
+		System.out.println("Hey Arya");//newly added
+		System.out.println("Hey Aishu");//newly added
+	}
+
+}
